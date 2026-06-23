a58154ec195cdf2e5eb036e1c62444b6c6e6183d
Zachary <baronzach962@gmail.com>
2026-06-23 16:21:47 -0400

    Menial

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 <h3>How does this work?</h3>
 
+Well the primary mechanism behind the map generation is the combinig of matrixes. 
 
+THE_MAP (per the code) constitutes to base determining matrix, holding identifiers like grass and dirt
+
+A seperate matrix is then generated using THE_MAP for things like tree generation and river generation
--- river_generation.lua
+++ river_generation.lua
@@ -1,114 +1,104 @@
 --River Generation--
 
-function to_middle(THE_MAP, river_points,x,y, iter)
-	local start_point = river_points[iter][1]
-	local end_point = river_points[iter][3]
-	--Function implementation--
-	local randomness = function() return not(math.random(1,12)>4) end
-	
 
-	local is_left = function(val,diff_value) return val==math.abs(diff_value) end
-	--End--
-	local diff_x = start_point[1] - end_point[1]
-	local diff_y = start_point[2] - end_point[2]
+function to_middle(THE_MAP, river_points, x, y, iter)
+    local start_point = river_points[iter][1]
+    local end_point = river_points[iter][3]
+    --Function implementation--
+    local randomness = function() return not (math.random(1, 12) > 4) end
 
 
-	THE_MAP[y][x]=3
-	return to_middle(THE_MAP, river_points,x,y, iter)
+    local is_left = function(val, diff_value) return val == math.abs(diff_value) end
+    --End--
+    local diff_x = start_point[1] - end_point[1]
+    local diff_y = start_point[2] - end_point[2]
 
+
+    THE_MAP[y][x] = 3
+    return nil
 end
 
 function to_end(THE_MAP, river_points)
 
 end
 
-
-
 function generate_path(THE_MAP, river_points)
-	current_mode = "start"
-	current_x = nil
-
+    current_mode = "start"
+    current_x = nil
 end
 
 function river_generation(THE_MAP, length, height)
-	--pair: start and end points--
-	height = height-1
-	local Axis = {"Top", "Bottom", "Left", "Right"}
-	local number_of_pairs = 2
-	for i=0, number_of_pairs do
-		--Random start point+axis--
-		local starter_axis = Axis[math.random(#Axis)]
-		if(starter_axis=="Top")then
-			x = math.random(1, length)
-			y=1
-		elseif(starter_axis=="Bottom")then
-			x=math.random(1, length)
-			y=height
-		elseif(starter_axis=="Left")then
-			x=1
-			y=math.random(1,height)
-		elseif(starter_axis=="Right")then
-			x=length
-			y=math.random(1,height)
-		end
+    --pair: start and end points--
+    height = height - 1
+    local Axis = { "Top", "Bottom", "Left", "Right" }
+    local number_of_pairs = 2
+    for i = 0, number_of_pairs do
+        --Random start point+axis--
+        local starter_axis = Axis[math.random(#Axis)]
+        if (starter_axis == "Top") then
+            x = math.random(1, length)
+            y = 1
+        elseif (starter_axis == "Bottom") then
+            x = math.random(1, length)
+            y = height
+        elseif (starter_axis == "Left") then
+            x = 1
+            y = math.random(1, height)
+        elseif (starter_axis == "Right") then
+            x = length
+            y = math.random(1, height)
+        end
 
-		end_x = math.random(1, length)
-		end_y = math.random(1,height)
+        end_x = math.random(1, length)
+        end_y = math.random(1, height)
 
-		--Actually altering the map now--
-		THE_MAP[y][x]=3
-
-
-		
-	end
+        --Actually altering the map now--
+        THE_MAP[y][x] = 3
+    end
 end
-
 
 function random_axis_point(length, height)
-	x=1
-	y=1
-	choices = {"TOP", "BOTTOM", "LEFT", "RIGHT"}
-	local choice = choices[math.random(#choices)]
-	if(choice=="TOP")then
-		x=math.random(1, length)
-		y=1	
-	end
+    x = 1
+    y = 1
+    choices = { "TOP", "BOTTOM", "LEFT", "RIGHT" }
+    local choice = choices[math.random(#choices)]
+    if (choice == "TOP") then
+        x = math.random(1, length)
+        y = 1
+    end
 
-	if(choice=="BOTTOM")then
-		x=math.random(1,length)
-		y=height
-	end
+    if (choice == "BOTTOM") then
+        x = math.random(1, length)
+        y = height
+    end
 
-	if(choice=="LEFT")then
-		x=1
-		y=math.random(1,height)
-	end
+    if (choice == "LEFT") then
+        x = 1
+        y = math.random(1, height)
+    end
 
-	if(choice=="RIGHT")then
-		x=length
-		y=math.random(1,height)
-	end
+    if (choice == "RIGHT") then
+        x = length
+        y = math.random(1, height)
+    end
 
-	return x,y
+    return x, y
 end
 
-
 function new_river_gen(THE_MAP, length, height) --Gives river_points, THE_MAP functions take care of the rest--
-	river_points = {}
-	local rivers=2
-	for i=1, rivers do
-		local start_x,start_y = random_axis_point(length, height)
-		local end_x, end_y = random_axis_point(length, height)
-		river_points[i] = {{start_x, start_y},{math.random(length), math.random(height)},{end_x, end_y}}
-	
-	end
-	
+    river_points = {}
+    local rivers = 2
+    for i = 1, rivers do
+        local start_x, start_y = random_axis_point(length, height)
+        local end_x, end_y = random_axis_point(length, height)
+        river_points[i] = { { start_x, start_y }, { math.random(length), math.random(height) }, { end_x, end_y } }
+    end
 
-	--Rendering here I guess?--
-	for i=1, rivers do
-		THE_MAP[river_points[i][1][2]][river_points[i][1][1]] = 3 --Start--
-		THE_MAP[river_points[i][2][2]][river_points[i][2][1]] = 3 --Middle--
-		THE_MAP[river_points[i][3][2]][river_points[i][3][1]] = 3 --End--
-	end
-	
+
+    --Rendering here I guess?--
+    for i = 1, rivers do
+        THE_MAP[river_points[i][1][2]][river_points[i][1][1]] = 3 --Start--
+        THE_MAP[river_points[i][2][2]][river_points[i][2][1]] = 3 --Middle--
+        THE_MAP[river_points[i][3][2]][river_points[i][3][1]] = 3 --End--
+    end
 end
